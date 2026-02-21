03b796661c421ec62dccda60348fb9309f83b22f
NotKhalid5 <khalidajr@icloud.com>
2026-02-20 20:13:55 -0500

    First Comm...Status Touch Down.

---
+++ src/Appointment.java
@@ -0,0 +1,71 @@
+public class Appointment {
+    private int patientID;
+    private String apptDate;
+    private String physician;
+    private double height;
+    private double weight;
+    private double temperature;
+    private int pulse;
+    private int bpSystolic;
+    private int bpDiastolic;
+    private String notes;
+
+    public Appointment() {
+        patientID = 0;
+        apptDate = null;
+        physician = null;
+        height = 0.0;
+        weight = 0.0;
+        temperature = 0.0;
+        pulse = 0;
+        bpSystolic = 0;
+        bpDiastolic = 0;
+        notes = null;
+    }
+
+    public int getPatientID() {return patientID;}
+
+    public void setPatientID(int ID) {this.patientID = ID;}
+
+    public String getApptDate() {return apptDate;}
+
+    public void setApptDate(String apptDate) {this.apptDate = apptDate;}
+
+    public String getPhysician() {return physician;}
+
+    public void setPhysician(String physician) {this.physician = physician;}
+
+    public double getHeight() {return height;}
+
+    public void setHeight(double height) {this.height = height;}
+
+    public double getWeight() {return weight;}
+
+    public void setWeight(double weight) {this.weight = weight;}
+
+    public double getTemperature() {return temperature;}
+
+    public void setTemperature(double temp) {this.temperature = temp;}
+
+    public int getPulse() {return pulse;}
+
+    public void setPulse(int pulse) {this.pulse = pulse;}
+
+    public int getBpDiastolic() {return bpDiastolic;}
+
+    public void setBpDiastolic(int bpd) {this.bpDiastolic = bpd;}
+
+    public int getBpSystolic() {return bpSystolic;}
+
+    public void setBpSystolic(int bps) {this.bpSystolic = bps; }
+
+    public String getNotes() {return notes;}
+
+    public void setNotes(String notes) {this.notes = notes;}
+
+
+    public String toString() {return patientID + "*" + apptDate +
+            "*" + physician + "*" + height + "*" + weight + "*"
+            + temperature + "*" + pulse + "*" + bpSystolic + "*" + bpDiastolic
+            + "*" + notes;}
+}
